a1d8b0a808096fc8409196f3b97c4c75267e4668
sushmithaNS <supriyasuresh.n@gmail.com>
2025-01-08 00:08:41 -0800

    all files

---
+++ src/page/home/Home.jsx
@@ -0,0 +1,64 @@
+import React from 'react'
+import './style.css'
+
+function Home() {
+  return (
+  <section className="home">
+        <p className='text-red'>OPINION</p>
+        <h1 className="big-heading">
+        What To Focus on at Microsoft Ignite <br /> This Year: Avoiding AI Catastrophes
+        </h1>
+        <br />
+        <p className='text-red'><big>By Rob Enderle - November 18, 2024 5:00 AM PT - <a href="mailto:bhaskarAntony@gmail.com" className='text-red'>Email Article</a></big></p>
+        <br />
+        <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQJweD2Ekz2XXf8dTFtW5eiRHq7EGPGHOLlrQ&s" alt=""  className='main-image'/>
+        <p><b>Chairman and CEO Satya Nadella speaks at Microsoft Ignite 2023. (Photo by Dan DeLong)</b></p>
+        <br />
+        <hr />
+        <p><big><a href="https://www.technewsworld.com/lead-generation-services" className='text-red' target="_blank"><b>Engage with IT decision makers and key influencers.</b></a></big></p>
+        <p className='content'>TechNewsWorld's B2B marketing services deliver sales-ready leads that shorten your sales process. Fill your funnel and grow revenue with our lead gen expertise.  <span className="text-red"> Get Started Today</span></p>
+        <br />
+        <p className="content big">
+        <span className="text-red">Microsoft Ignite</span> is on this week. It’s the company’s massive event focused on IT professionals. While you can attend this event remotely, you should consider attending in person this year because I still see far more AI failures than successes. When AI is deployed successfully, it has a significant positive impact on productivity, yet quality appears to be going in the wrong direction. But not for everyone.
+        </p> <br />
+
+        <p className="content">
+        Shows like Microsoft Ignite are full of information on products and services. The event is attended by many IT professionals in different industries, with varying levels of AI deployment and very different paths to success.
+        </p> 
+        <br />
+        <p className='content'>
+        With the intense focus on all things AI, and Microsoft Copilot being one of the most widely spread AI tools, being able to talk to the people who have been successful with this tool, which vendors they worked with to deploy it successfully, and what practices helped their success become golden knowledge.
+        </p>
+        <br />
+        <p className="content">
+        Let’s talk about why this year’s Ignite is important not only to attend — but to attend in person. We’ll close with my Product of the Week: Bluesky, the social media platform I hear people are having the best success with as they move from Twitter/X.
+        </p>
+        <br />
+        <h2>The Value of Relationships</h2>
+        <br />
+        <p className="content">
+        Building strong vendor and peer relationships is essential for AI success, especially when navigating the complexities of new technology like Microsoft Copilot, which appears to be the most successful AI offering in the market so far. It has enormous productivity-improving value to every product it enhances.
+        </p>
+        <br />
+        <p className="content">
+        Because Microsoft is a platform, tools, and cloud company, its tools tend to be relatively unfocused in terms of industry or geography. This broad applicability doesn’t preclude tuning; it indicates the tools can be useful to almost every company, from enterprises to small businesses.
+        </p>
+        <br />
+        <p className="content">
+        While smaller companies will likely use AI as a service, enterprises are increasingly likely to use tools hosted on their own hardware or in a hybrid approach where sensitive information is retained, AI is accessed on-premises, and less secure information and implementation that don’t require low latency can be cloud-deployed.
+        </p>
+        <br />
+        <p className="content">
+        The issue to address is that service providers, OEMs, and most companies trying to ramp up AI don’t know enough about it to succeed. However, the people who understand AI and have had successful AI deployments (particularly the early adopters Microsoft assisted) will be at Ignite. Meeting these experts could make a difference in whether you overspend on these tools and help avoid common mistakes by learning from the mistakes of others and being able to delineate the hype from reality.
+        </p>
+        <br />
+        <h2>Genuine Value vs. Exaggerated Claims</h2>
+        <br />
+        <p className="content">
+        I learned this lesson a few decades ago when Microsoft held an event arguing that its Windows NT servers could replace mainframes. When I had a chance to talk to two of Microsoft’s reference accounts, it became clear that Windows NT wasn’t ready to replace mainframes for most organizations.
+        </p>
+  </section>
+  )
+}
+
+export default Home
